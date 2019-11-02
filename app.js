73394fee2939c5afe7cdcd877b8882eee2f37fa5
Sara Martinez <s_martinez_1990@outlook.com>
2019-11-02 03:35:47 -0400

    app debugging

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-// var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function(response) {
-//       console.log(response);
-//     });
-
 $(document).ready(function() {
+    console.log("is working");
     
-    const animals =[ "cat", "dog", "rabbit", "snake", "fish", "horse"];
+    var animals =[ "cat", "dog", "rabbit", "snake", "fish", "horse"];
 
     function renderBtn () {
         $("#animalBtn").empty();
@@ -28,29 +21,45 @@ $(document).ready(function() {
         renderBtn();
     });
 
-    $("#button").on("click", function () {
+    $(document).on("click", ".animalBtn", function () {
         var animal = $(this).attr("data-animals");
+        $("#animals").empty();
+        $(".animal-button").removeClass("active");
+        $(this).addClass("active");
+        var type=$(this).attr("data-type");
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+        //xbZ4W8BLBjNYVW6LTKVNoNQ9qkuwMdwi
+
              $.ajax({
              url: queryURL,
              method: "GET"
              }).then(function(response) {
-                 console.log(queryURL);
-                 console.log(response);
+                 var results=response.data
+                 for (var i=0; i<results.length; i++) {
+                    var animalDiv = $("<div class=\"animal-item\">");
 
-                 for (var i=0; i<response.length; i++) {
-                     var animalDiv =$("<div>");
-                     var p=$("<p>").text("Rating: " + response[i].rating);
-                     var animalImage=$("<img>");
-                     animalImage.attr("src",response[i].images.fixed_height.url);
-
-                     animalDiv.append(p);
+                    var rating = results[i].rating;
+          
+                    var p = $("<p>").text("Rating: " + rating);
+          
+                    var animated = results[i].images.fixed_height.url;
+                    var still = results[i].images.fixed_height_still.url;
+          
+                    var animalImage = $("<img>");
+                    animalImage.attr("src", still);
+                    animalImage.attr("data-still", still);
+                    animalImage.attr("data-animate", animated);
+                    animalImage.attr("data-state", "still");
+                    animalImage.addClass("animal-image");
+          
+                    animalDiv.append(p);
                     animalDiv.append(animalImage);
-
-                    $("#animals").prepend(animalDiv);
+          
+                    $("#animals").append(animalDiv);
                     }
            });
 
     })
+    
     renderBtn(); 
 })
