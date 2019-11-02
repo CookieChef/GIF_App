aca5555518aabf3c0195f246f570d455d5461181
Sara Martinez <s_martinez_1990@outlook.com>
2019-11-02 09:35:38 -0400

    got the ajax call working!

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // $(document).ready(function() {
 //     console.log("is working");
-    
+
 //     var animals =[ "cat", "dog", "rabbit", "snake", "fish", "horse"];
 
 //     function renderBtn () {
@@ -36,37 +36,62 @@
 //              }).then(function(response) {
 //                  var results=response.data
 //                  for (var i=0; i<results.length; i++) {
-                    
+
 //                     }
 //            });
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     console.log("its working!!");
 
     var animals = ["cat", "dog", "fish", "horse"];
 
-        //function that takes the the elements of the array and creates a button for them
-        function renderBtn () {
-            $("#animalBtn").empty();
-                for (var i=0; i<animals.length; i++) {
-                    var a=$("<button>");
-                    a.addClass("animal");
-                    a.attr("data-animal", animals[i]);
-                    a.text(animals[i]);
-                    $("#animalBtn").append(a);
+    //function that takes the the elements of the array and creates a button for them
+    function renderBtn() {
+        $("#animalBtn").empty();
+        for (var i = 0; i < animals.length; i++) {
+            var a = $("<button>");
+            a.addClass("animal");
+            a.attr("data-animal", animals[i]);
+            a.text(animals[i]);
+            $("#animalBtn").append(a);
 
-                }
         }
+    }
 
-        //when the user hits sumbit this code snippet takes the user input and pushes is into the array
-        $("#add-animal").on("click", function(event){
-            event.preventDefault();
-            var animal = $("#animal-input").val().trim();
-            animals.push(animal);
-            renderBtn();
-        })
+    //when the user hits sumbit this code snippet takes the user input and pushes is into the array
+    $("#add-animal").on("click", function (event) {
+        event.preventDefault();
+        var animal = $("#animal-input").val().trim();
+        animals.push(animal);
         renderBtn();
+    })
+
+    $(document).on("click", "#animalBtn", function () {
+        $("#animals").empty();
+        $("#animalBtn").removeClass("active");
+        $(this).addClass("active");
+
+        var type = $(this).attr("data-type");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+        //makes the call to get a response 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+                var results = response.data;
+                //this following code will loop throgh the response the ajax call got
+                //and it will create an animalDiv class
+                for (var i=0; i<results.length; i++) {
+                    var animalDiv = $("<div class=\"animal-item\">");
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                }
+            })
+    })
+
+    renderBtn();
 })
 
 
