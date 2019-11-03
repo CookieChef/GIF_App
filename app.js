a6d4d6deb13c227c9557c43d554f15cf41f80b2d
Sara Martinez <s_martinez_1990@outlook.com>
2019-11-02 22:13:53 -0400

    logic complete

--- app.js
+++ app.js
@@ -4,33 +4,25 @@ $(document).ready(function () {
     var animals = ["cat", "dog", "fish", "horse"];
 
     //function that takes the the elements of the array and creates a button for them
-    function renderBtn() {
-        $("#animalBtn").empty();
-        for (var i = 0; i < animals.length; i++) {
+    function renderBtn(arrayToUse, classToAdd, areatoAddTo) {
+        $(areatoAddTo).empty();
+        for (var i = 0; i < arrayToUse.length; i++) {
             var a = $("<button>");
-            a.addClass("animal");
-            a.attr("data-animal", animals[i]);
-            a.text(animals[i]);
-            $("#animalBtn").append(a);
+            a.addClass(classToAdd);
+            a.attr("data-type", arrayToUse[i]);
+            a.text(arrayToUse[i]);
+            $(areatoAddTo).append(a);
 
         }
     }
 
-    //when the user hits sumbit this code snippet takes the user input and pushes is into the array
-    $("#add-animal").on("click", function (event) {
-        event.preventDefault();
-        var animal = $("#animal-input").val().trim();
-        animals.push(animal);
-        renderBtn();
-    })
-
-    $(document).on("click", "#animalBtn", function () {
+    $(document).on("click", ".animal-button", function () {
         $("#animals").empty();
-        $("#animalBtn").removeClass("active");
+        $(".animal-button").removeClass("active");
         $(this).addClass("active");
 
         var type = $(this).attr("data-type");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=5";
 
         //makes the call to get a response 
         $.ajax({
@@ -66,7 +58,30 @@ $(document).ready(function () {
                 }
             });
     });
+
+    //when the user hits sumbit this code snippet takes the user input and pushes is into the array
+    $("#add-animal").on("click", function (event) {
+        event.preventDefault();
+        var animal = $("#animal-input").val().trim();
+        animals.push(animal);
+        renderBtn();
+    })
     
+    $(document).on("click", ".animal-image", function() {
+
+        var state = $(this).attr("data-state");
+    
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        }
+        else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
+
+
     $("#add-animal").on("click", function (event) {
         event.preventDefault();
         var newAnimal = $("input").eq(0).val();
@@ -74,13 +89,10 @@ $(document).ready(function () {
             if (newAnimal.length > 2) {
                 animals.push(newAnimal);
             }
-            renderBtn();
+            renderBtn(animals, "animal-button", "#animal-buttons");
     });
 
-    
-
-
-    renderBtn();
+    renderBtn(animals, "animal-button", "#animal-buttons");
 })
 
 
