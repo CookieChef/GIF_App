0a18d794f87de60fb8b1d1f095ff469a1522c557
Sara Martinez <s_martinez_1990@outlook.com>
2019-11-07 16:46:11 -0500

    almost done

--- app.js
+++ app.js
@@ -1,88 +1,61 @@
-$(document).ready(function() {
-    console.log("its working!!");
-
+$(document).ready(function () {
+  console.log("app.js is linked to the index.html");
     var animals = ["cat", "dog", "fish", "horse"];
-  
-    //function that takes the the elements of the array and creates a button for them
-    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
-      $(areaToAddTo).empty();
-      for (var i = 0; i < arrayToUse.length; i++) {
+
+    var makeBtn = function () {
+      $("#dump_buttons").empty();
+      for (var i=0; i<animals.length; i++) {
         var a = $("<button class=\"btn btn-outline-dark\">");
-        a.addClass(classToAdd);
-        a.attr("data-type", arrayToUse[i]);
-        a.text(arrayToUse[i]);
-        $(areaToAddTo).append(a);
+        a.addClass("<button>");
+        a.attr("data-type", animals[i]);
+        a.text(animals[i]);
+        $("#dump_buttons").append(a);
       }
-  
     }
-    
-    //Here we start to make our call to get a response when the user clicks on the button
-    $(document).on("click", ".animal-button", function() {
-      $("#animals").empty();
-      $(".animal-button").removeClass("active");
-      $(this).addClass("active");
+    makeBtn();
+
+
+$(document).on("click", "#dump_buttons", function () {
+  var type = $(this).attr("data-type");
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=5";
+  $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+            .then(function(response) {
+              var results = response.data;
+              console.log(results);
+              for (var i = 0; i < results.length; i++) {
+                var animalDiv = $("<div>").addClass("animal_disp");
+                $("#image_dump").append(animalDiv);
+                var rating = results[i].rating;
+                var p=$("<p>").text("Rating: " + rating);
+                var animated = results[i].images.fixed_height.url;
+                
+                var animalImage = $("<img>");
+                animalImage.attr("src", animated);
+                animalImage.addClass("animal_img");
+
+
+                animalDiv.append(p);
+                animalDiv.append(animalImage);
+
+                $("#animal_dump").append(animalDiv);
+              }
+                  
+
+})
+})
+
+$("#add_animal").on("click", function(event){
+  event.preventDefault();
+
+  var newAnimal = $("#animal-input").val();
+  animals.push(newAnimal);
+  $("#animal-input").val("");
   
-      var type = $(this).attr("data-type");
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=5";
-  
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        .then(function(response) {
-          var results = response.data;
-            //here we loop through the results array and we add a class to each element of the array 
-          for (var i = 0; i < results.length; i++) {
-            var animalDiv = $("<div class=\"animal-item\">");
-  
-            var rating = results[i].rating;
-  
-            var p = $("<p>").text("Rating: " + rating);
-  
-            var animated = results[i].images.fixed_height.url;
-            var still = results[i].images.fixed_height_still.url;
-            //here we create a image tag and the attributes for the image to be active or still and add a class to it
-            var animalImage = $("<img>");
-            animalImage.attr("src", still);
-            animalImage.attr("data-still", still);
-            animalImage.attr("data-animate", animated);
-            animalImage.attr("data-state", "still");
-            animalImage.addClass("animal-image");
-  
-            animalDiv.append(p);
-            animalDiv.append(animalImage);
-  
-            $("#animals").append(animalDiv);
-          }
-        });
-    });
-    
-    //this code allows the images to be active or still when the user clicks on them 
-    $(document).on("click", ".animal-image", function() {
-  
-      var state = $(this).attr("data-state");
-  
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      }
-      else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
-  
-    $("#add-animal").on("click", function(event) {
-      event.preventDefault();
-      var newAnimal = $("input").eq(0).val();
-  
-      if (newAnimal.length > 2) {
-        animals.push(newAnimal);
-      }
-  
-      populateButtons(animals, "animal-button", "#animal-buttons");
-  
-    });
-  
-    populateButtons(animals, "animal-button", "#animal-buttons");
-  });
+  makeBtn();
+})
+//========================
+})
+
